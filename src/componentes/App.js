dbe3d5404fbd7bcbeea8fa94df8d08ce0b7b1b58
Florencia Nodar <fnodar@algeiba.com>
2018-12-25 16:50:57 -0300

    se agregó componente de formulario.js con los campos  correspondientes

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import '../css/app.css';
 import Header from './Header';
+import Formulario from './Formulario';
+
+
 class App extends Component {
   render() {
     return (
@@ -8,7 +11,9 @@ class App extends Component {
       <Header titulo='Gasto semanal' />
       <div className="contenido-principal contenido">
          <div className="row">
-           <div className="one-half column"></div>
+           <div className="one-half column">
+              <Formulario />
+           </div>
            <div className="one-half column"></div>
          </div>
       </div>
