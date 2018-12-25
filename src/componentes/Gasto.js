39a138e9db1396276b23664c7930372053df21c7
Florencia Nodar <fnodar@algeiba.com>
2018-12-25 19:19:49 -0300

    Se agregaron los gastos renderizados, recorriendo con un Object.key cada objeto de la lista que recibe

---
+++ src/componentes/Gasto.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Gasto extends Component{
+    render(){
+        return(
+        <p>Desde gasto.js</p>
+        )
+    }
+}
+
+export default Gasto;
--- src/componentes/Listado.js
+++ src/componentes/Listado.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import Gasto from './Gasto';
 
 class Listado extends Component{
 render(){
     return(
         <div className="gastos-realizados">
         <h2>Listado</h2>
-            {console.log(this.props.gastos)}
+        {Object.keys(this.props.gastos).map(key=>(
+            <Gasto key={key} gasto={this.props.gastos[key]} />
+         ))} {/*Recorro el objeto this.props.gastos y con el Object.key lo recorre, y lo va mostrando cada item en distinto objeto */}
+         {/*El key es para ver que elemento estoy renderizando*/}
         </div>
     )
 }
