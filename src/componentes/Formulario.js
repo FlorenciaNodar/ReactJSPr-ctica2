a8670397cea01bb6c839b830dcacb62224e3fe25
Florencia Nodar <fnodar@algeiba.com>
2018-12-25 17:04:53 -0300

    Se leen los campos del formulario, y se guardan en un objeto, y se resetea el formulario

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
 
 class FormularioGasto extends Component{
+
+    //refs para los campos del formulario
+    nombreGasto = React.createRef();
+    cantidadGasto = React.createRef();
+
+    crearGastos = (e) =>{
+        //prevenir el default
+        e.preventDefault();
+        //crear el objeto con los datos
+        const gasto={
+            nombreGasto: this.nombreGasto.current.value,
+            cantidadGasto: this.cantidadGasto.current.value
+        }
+
+
+        console.log(gasto);
+        // agregarlo y enviarlo por props
+        //resetear el formulario (optional)
+        e.currentTarget.reset();
+    }
     render(){
         return(
-            <form>
+            <form onSubmit={this.crearGastos} >
                 <h2>Agrega tus gastos aqui</h2>
                 <div className="campo">
                     <label>Nombre Gasto</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. Transporte" />
+                    <input ref={this.nombreGasto} className="u-full-width" type="text" placeholder="Ej. Transporte" />
                 </div>
                 <div className="campo">
                     <label>Cantidad</label>
-                    <input className="u-full-width" type="text" placeholder="Ej. 300" />
+                    <input ref={this.cantidadGasto} className="u-full-width" type="text" placeholder="Ej. 300" />
                 </div>
 
                 <input className="button-primary u-full-width" type="submit" value="Agregar" />
